b7e05937bebce8acc13e0aa1ca1384963fd8c319
Igzanotto <ignaciozanotto@gmail.com>
2024-05-06 23:38:01 -0300

    middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+// esto ni idea que esta haciendo pero lo indica aca
+// https://supabase.com/docs/guides/auth/server-side/nextjs
+
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
