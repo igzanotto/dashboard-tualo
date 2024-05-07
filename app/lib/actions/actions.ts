7025efadf6f0d49e6fae7d6c411ff237b8db3b09
Igzanotto <ignaciozanotto@gmail.com>
2024-05-07 17:36:48 -0300

    data actions test

--- app/lib/actions/actions.ts
+++ app/lib/actions/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { createClient } from '@/utils/supabase/server';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -35,4 +36,19 @@ export async function createInvoice(formData: FormData) {
 }
 
 
+export async function addCompany(name:string) {
+  const supabase = createClient();
+  try {
+    const { data:companies, error } = await supabase.from('companies').insert({ name });
 
+    if (error) {
+      throw error;
+    }
+    
+    console.log('company created:', companies);
+    return companies;
+  } catch (error) {
+    console.error('Failed to add company:', error);
+    throw new Error('Failed to add company.');
+  }
+}
