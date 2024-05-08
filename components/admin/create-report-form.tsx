a9ad697f63a7d51915b189566806cf8da0149aa1
Igzanotto <ignaciozanotto@gmail.com>
2024-05-08 18:53:35 -0300

    report form

---
+++ components/admin/create-report-form.tsx
@@ -0,0 +1,8 @@
+export default function Form() {
+    return (
+        // <form action={createReport}>
+        //     <input type="text" name="company" placeholder='Nombre del negocio' className='rounded-xl bg-slate-100 px-2'/>
+        //     <button type="submit" className='p-3 rounded-xl bg-blue-500 text-white'>Agregar negocio</button>
+        // </form>
+    );
+}
