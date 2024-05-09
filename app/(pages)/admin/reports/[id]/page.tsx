46f3580bea557d5e3c50925ff839e4cdfc3a1b53
Manuel-latorre <manuel.latorre11@gmail.com>
2024-05-09 11:32:43 -0300

    add report page

--- app/(pages)/admin/reports/[id]/page.tsx
+++ app/(pages)/admin/reports/[id]/page.tsx
@@ -7,8 +7,23 @@ export default async function ReportPage({ params }: { params: { id: string } })
 
     return(
         <div className="flex flex-col gap-3">
-            <h1 className="text-xl md:text-2xl xl:text-5xl">{report.id}</h1>
-            <p>{report.month}</p>
+            <h1 className={`text-2xl`}>Reporte de <span className="capitalize">{report.month}</span></h1>
+
+            <div className="flex flex-col gap-8 mt-10">
+                <div>
+                    <p className="text-4xl mb-4">Resumen</p>
+                    {report.buisness_resume ? <p>{report.buisness_resume}</p> : <p>Vacío</p>}
+                </div>
+                <div>
+                    <p className="text-4xl mb-4">Objetivos</p>
+                    {report.goals ? <p>{report.goals}</p> : <p>Vacío</p>}
+                </div>
+                <div>
+                    <p className="text-4xl mb-4">Análisis</p>
+                    {report.analysis ? <p>{report.analysis}</p> : <p>Vacío</p>}
+                </div>
+            </div>
+            {/* <p>{report.buisness_id}</p> */}
         </div>
     )
 }
