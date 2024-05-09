7db12c87ed6ecc1950221f26280010109fdb9cb8
Igzanotto <ignaciozanotto@gmail.com>
2024-05-09 09:13:46 -0300

    company actions complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,70 +1,24 @@
 'use server';
- 
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/server';
-import { NextRequest } from 'next/server';
 
-const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
-});
- 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
- 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+export async function createCompany(formData: FormData) {
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-
-    // clear this cache and trigger a new request to the server for the path to see the new invoice
-    revalidatePath('/dashboard/negocios');
-
-    redirect('/dashboard/negocios');
-}
-
-// creo que este no se esta usando
-export async function addCompany(name:string) {
+  const name = formData.get('name');
+  const description = formData.get('description');
   const supabase = createClient();
-  try {
-    const { data:companies, error } = await supabase.from('companies').insert({ name });
 
-    if (error) {
-      throw error;
-    }
-    
-    console.log('company created:', companies);
-    return companies;
-  } catch (error) {
-    console.error('Failed to add company:', error);
-    revalidatePath("/pages/admin/negocios")
+  const { data: companies, error } = await supabase.from('companies').insert({ name, description });
 
-    throw new Error('Failed to add company.');
+  if (error) {
+    throw error;
   }
-}
 
-export async function create (formData:FormData) {
-  try {
-    const company = formData.get("company")
-    await addCompany(company as string);
-    console.log('Company added successfully!', company);
-    revalidatePath("/pages/admin/negocios")
-  } catch (error) {
-    console.log(error);
-  }
+  // clear this cache and trigger a new request to the server for the path to see the new company
+  revalidatePath('/pages/admin/negocios');
+
+  redirect('/pages/admin/negocios');
 }
 
 export async function createReport(formData:FormData) {
--- components/admin/form-companies.tsx
+++ components/admin/form-companies.tsx
@@ -1,31 +1,13 @@
-"use client"
-
-import { create } from "@/app/lib/actions";
-import { redirect } from "next/navigation";
-import { useRef } from "react";
-import { toast } from "sonner";
-
+import { createCompany } from "@/app/lib/actions";
 
 export default async function AddCompanyForm() {
 
-  const formRef = useRef<HTMLFormElement>(null)
-
-
   return (
-    <form 
-      ref={formRef}
-      action={
-      async formData => {const company = await create(formData);
-        console.log("Company added successfully", company);
-        toast.success("Negocio agregado correctamente");
-        formRef.current?.reset();
-        redirect("/pages/admin/negocios")
-      }
-      
-      } 
+    <form action={createCompany}
       className='flex flex-col gap-4 w-[100%]'
       >
-      <input type="text" name="company" placeholder='Nombre del negocio' className='rounded-xl bg-slate-100 px-2'/>
+      <input type="text" name="name" placeholder='Nombre del negocio' className='rounded-xl bg-slate-100 px-2'/>
+      <input type="text" name="description" placeholder='Descripción del negocio' className='rounded-xl bg-slate-100 px-2'/>
       <button type="submit" className='p-3 rounded-xl bg-blue-500 text-white'>Agregar negocio</button>
     </form>
   );
