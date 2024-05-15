c1a6646e4c36f329f8fc3f158ba9d706853c9e59
Igzanotto <ignaciozanotto@gmail.com>
2024-05-15 11:50:08 -0300

    report page in admin and dashboard

--- app/(pages)/admin/reports/[id]/page.tsx
+++ app/(pages)/admin/reports/[id]/page.tsx
@@ -1,46 +1,41 @@
-import { fetchReportById, fetchReportsByBusinesses,  } from "@/app/lib/data";
+import { fetchReportById } from '@/app/lib/data';
 
-
-export default async function ReportPage({ params }: { params: { id: string } }){
+export default async function ReportPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = params.id;
-  const report = await fetchReportById(id)
-     
-  const reportsWithBusinesses = await fetchReportsByBusinesses();
+  const report = await fetchReportById(id);
+  const businessName = report.business.name;
 
-  const matchedReport = reportsWithBusinesses.find(idBusiness => idBusiness.id === report.id);
-  console.log(matchedReport);
-  
+  return (
+    <div className="flex flex-col gap-3">
+      <h1 className={`text-2xl`}>
+        Reporte de <span className="capitalize">{report.month}</span>
+      </h1>
+      <div>
+        <p className="mb-4 text-4xl">{businessName}</p>
+      </div>
 
-  const businessName = matchedReport ? matchedReport.business_id.name : 'No encontrado';
-  console.log(businessName);
-  
-    return (
-      <div className="flex flex-col gap-3">
-        <h1 className={`text-2xl`}>
-          Reporte de <span className="capitalize">{report.month}</span>
-        </h1>
+      <div className="mt-10 flex flex-col gap-8">
         <div>
-          <p className="mb-4 text-4xl">{businessName}</p>
+          <p className="mb-4 text-4xl">Resumen</p>
+          {report.business_resume ? (
+            <p>{report.business_resume}</p>
+          ) : (
+            <p>Vacío</p>
+          )}
         </div>
-
-        <div className="mt-10 flex flex-col gap-8">
-          <div>
-            <p className="mb-4 text-4xl">Resumen</p>
-            {report.business_resume ? (
-              <p>{report.business_resume}</p>
-            ) : (
-              <p>Vacío</p>
-            )}
-          </div>
-          <div>
-            <p className="mb-4 text-4xl">Objetivos</p>
-            {report.goals ? <p>{report.goals}</p> : <p>Vacío</p>}
-          </div>
-          <div>
-            <p className="mb-4 text-4xl">Análisis</p>
-            {report.analysis ? <p>{report.analysis}</p> : <p>Vacío</p>}
-          </div>
+        <div>
+          <p className="mb-4 text-4xl">Objetivos</p>
+          {report.goals ? <p>{report.goals}</p> : <p>Vacío</p>}
+        </div>
+        <div>
+          <p className="mb-4 text-4xl">Análisis</p>
+          {report.analysis ? <p>{report.analysis}</p> : <p>Vacío</p>}
         </div>
       </div>
-    );
+    </div>
+  );
 }
