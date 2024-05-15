3e9b5f13bd15a61dffbeb22233b713dbf280142f
Igzanotto <ignaciozanotto@gmail.com>
2024-05-15 09:18:19 -0300

    bugfix

--- app/(pages)/dashboard/layout.tsx
+++ app/(pages)/dashboard/layout.tsx
@@ -1,12 +1,11 @@
-import SideNav from "@/components/dashboard/sidenav";
-import ReportsTableDashboard from "@/components/dashboard/table-reports";
-import { InvoicesTableSkeleton } from "@/components/skeletons";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { Suspense } from "react";
+import SideNav from '@/components/dashboard/sidenav';
+import ReportsTableDashboard from '@/components/dashboard/table-reports';
+import { InvoicesTableSkeleton } from '@/components/skeletons';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 
-
-export interface ReportsPageProps {
+interface ReportsPageProps {
   params: {
     id: string;
   };
@@ -16,30 +15,39 @@ export interface ReportsPageProps {
   };
 }
 
-export default async function Layout({ children, searchParams }: { children: React.ReactNode; searchParams?: ReportsPageProps["searchParams"]; }) {
+export default async function Layout({
+  children,
+  searchParams,
+}: {
+  children: React.ReactNode;
+  searchParams?: ReportsPageProps['searchParams'];
+}) {
   const supabase = createClient();
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
-const {
-  data: { user },
-} = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-if (!user) {
-  return redirect("/login");
-}
+  if (!user) {
+    return redirect('/login');
+  }
 
-return (
-  <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-    <div className="w-full flex-none md:w-64">
-      <SideNav />
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        <Suspense
+          key={query + currentPage}
+          fallback={<InvoicesTableSkeleton />}
+        >
+          <ReportsTableDashboard query={query} currentPage={currentPage} />
+        </Suspense>
+        {children}
+      </div>
     </div>
-    <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <ReportsTableDashboard query={query} currentPage={currentPage}/>
-      </Suspense>
-      {children}
-    </div>
-  </div>
-);
+  );
 }
