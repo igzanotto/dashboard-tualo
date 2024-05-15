7609bf542b4dada8d0d8b69efe79904df62baf65
Igzanotto <ignaciozanotto@gmail.com>
2024-05-15 09:27:54 -0300

    login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import Image from 'next/image';
-import ApiForm from '@/components/admin/api-form';
 import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
 
