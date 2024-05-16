125130fc4f8d19411b5ea28b3eafc6267c7f9bb0
Igzanotto <ignaciozanotto@gmail.com>
2024-05-16 09:55:58 -0300

    data

--- components/charts/spChart.tsx
+++ components/charts/spChart.tsx
@@ -7,48 +7,48 @@ const MyChart = () => {
   const [chartOptions, setChartOptions] = useState({
     chart: {
       type: 'rangeBar',
-      height: 350
+      height: 350,
     },
     plotOptions: {
       bar: {
-        horizontal: false
-      }
+        horizontal: false,
+      },
     },
     dataLabels: {
-      enabled: true
+      enabled: true,
     },
-    // colors: ['#FF5733', '#33FF57', '#7364BE', '#090715'] // Colores personalizados
-
+    colors: ['#F44336'],
   });
 
   const [chartSeries, setChartSeries] = useState([
     {
-      data: [{
-        x: 'TEAM A',
-        y: [0, 96],
-        strokeColor: '#775DD0'
-      },
-      {
-        x: 'TEAM B',
-        y: [96, 78]
-      },
-      {
-        x: 'TEAM C',
-        y: [78, 56]
-      },
-      {
-        x: 'TEAM D',
-        y: [0, 96]
-      },
-      {
-        x: 'TEAM E',
-        y: [96, 78]
-      },
-      {
-        x: 'TEAM F',
-        y: [78, 56]
-      }]
-    }
+      data: [
+        {
+          x: 'TEAM A',
+          y: [0, 96],
+        },
+        {
+          x: 'TEAM B',
+          y: [96, 78],
+        },
+        {
+          x: 'TEAM C',
+          y: [78, 56],
+        },
+        {
+          x: 'TEAM D',
+          y: [0, 96],
+        },
+        {
+          x: 'TEAM E',
+          y: [96, 78],
+        },
+        {
+          x: 'TEAM F',
+          y: [78, 56],
+        },
+      ],
+    },
   ]);
 
   // Renderizar el componente del gráfico
