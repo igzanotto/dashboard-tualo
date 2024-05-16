2ef67c01ef239a42665afcd127835a1db3f740f4
Igzanotto <ignaciozanotto@gmail.com>
2024-05-16 10:46:23 -0300

    bugfix

--- components/admin/reports/table.tsx
+++ components/admin/reports/table.tsx
@@ -14,7 +14,7 @@ export default async function ReportsTable({ business_id}:ReportsTableProps) {
   return (
     <div className="flow-root">
       <div className='flex flex-col gap-2 w-full border-1 bg-gray-50 rounded-xl p-4'>
-        {reports.map(report => (
+        {reports && reports.map(report => (
           <div className='border-2 border-slate-200 p-2 rounded-xl' key={report.id}>
             <Link href={`/admin/reports/${report.id}`} className='flex items-center gap-2'>
               <p>{report.month}</p>
