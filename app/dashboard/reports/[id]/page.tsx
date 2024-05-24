3457f9feec4b5ddff734c4d5c4ff391039753963
Manuel-latorre <manuel.latorre11@gmail.com>
2024-05-23 21:25:23 -0300

    responsive cards

--- app/dashboard/reports/[id]/page.tsx
+++ app/dashboard/reports/[id]/page.tsx
@@ -2,6 +2,7 @@ import ChartEmbed from '@/components/charts/ChartEmbed';
 import ModalDashboard from '@/components/modal/Modal';
 import { fetchReportById } from '@/lib/data';
 import { FolderIcon, LightBulbIcon } from '@heroicons/react/24/outline';
+import { GoalIcon, InfoIcon } from 'lucide-react';
 
 export default async function ReportPage({
   params,
@@ -62,15 +63,14 @@ export default async function ReportPage({
       <div>
         <p className="mb-4 text-4xl">{report.business.name}</p>
       </div>
-      <div className="flex items-center gap-8">
-        <div className="flex w-[400px] h-[180px] flex-col gap-3 rounded-xl bg-[#2c197f] p-3 shadow-2xl">
+      <div className="flex items-center xl:gap-6 2xl:gap-8">
+        <div className="flex w-[400px] 2xl:h-[180px] h-[200px] flex-col gap-3 rounded-xl bg-emerald-500 p-3 shadow-2xl">
           <div className="flex items-center gap-2 text-2xl font-medium text-white">
             <FolderIcon width={30} height={30} />
             <p>Resumen</p>
           </div>
-          <p className="font-medium text-zinc-100">
-            En tu resumen verás una descripción detallada de tu negocio. A
-            continuación, se abordan diversos aspectos clave del negocio.
+          <p className="font-medium text-zinc-100 w-full">
+            En tu resumen verás una descripción detallada de tu negocio donde se abordan diversos aspectos clave.
           </p>
           <ModalDashboard
             name={report.business.name}
@@ -78,7 +78,7 @@ export default async function ReportPage({
           />
         </div>
         {/* bg-gradient-to-r from-[#4C30C5] to-[#39AEFF] */}
-        <div className="flex w-[400px] h-[180px] flex-col gap-3 rounded-xl bg-[#4b31be] p-3 shadow-2xl">
+        <div className="flex w-[400px] 2xl:h-[180px] h-[200px]  flex-col gap-3 rounded-xl bg-yellow-500 p-3 shadow-2xl">
           <div className="flex items-center gap-2 text-2xl font-medium text-white">
             <LightBulbIcon width={30} height={30} />
             <p>Recomendaciones</p>
@@ -95,9 +95,9 @@ export default async function ReportPage({
           />
         </div>
 
-        <div className="flex w-[400px] h-[180px] flex-col gap-3 rounded-xl bg-[#5a36fa] p-3 shadow-2xl">
+        <div className="flex w-[400px] 2xl:h-[180px] h-[200px]  flex-col gap-3 rounded-xl bg-[#5a36fa] p-3 shadow-2xl">
           <div className="flex items-center gap-2 text-2xl font-medium text-white">
-            <LightBulbIcon width={30} height={30} />
+            <GoalIcon width={30} height={30} />
             <p>Metas financieras</p>
           </div>
           <p className="font-medium text-zinc-100">
@@ -151,50 +151,9 @@ export default async function ReportPage({
                     </p>
                     <div className="flex items-start gap-2">
                       <div className="relative translate-y-0.5">
-                        <svg
-                          width={20}
-                          height={20}
-                          viewBox="0 0 48 48"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                          <g
-                            id="SVGRepo_tracerCarrier"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          ></g>
-                          <g id="SVGRepo_iconCarrier">
-                            {' '}
-                            <rect
-                              width="48"
-                              height="48"
-                              fill="white"
-                              fill-opacity="0.01"
-                            ></rect>{' '}
-                            <path
-                              d="M24 44C29.5228 44 34.5228 41.7614 38.1421 38.1421C41.7614 34.5228 44 29.5228 44 24C44 18.4772 41.7614 13.4772 38.1421 9.85786C34.5228 6.23858 29.5228 4 24 4C18.4772 4 13.4772 6.23858 9.85786 9.85786C6.23858 13.4772 4 18.4772 4 24C4 29.5228 6.23858 34.5228 9.85786 38.1421C13.4772 41.7614 18.4772 44 24 44Z"
-                              fill="#2F88FF"
-                              stroke="#000000"
-                              stroke-width="4"
-                              stroke-linejoin="round"
-                            ></path>{' '}
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M24 37C25.3807 37 26.5 35.8807 26.5 34.5C26.5 33.1193 25.3807 32 24 32C22.6193 32 21.5 33.1193 21.5 34.5C21.5 35.8807 22.6193 37 24 37Z"
-                              fill="white"
-                            ></path>{' '}
-                            <path
-                              d="M24 12V28"
-                              stroke="white"
-                              stroke-width="4"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            ></path>{' '}
-                          </g>
-                        </svg>
+                        
                       </div>
+                        <InfoIcon width={20} height={20}/>
                       <p>
                         Esta gráfica se lee de izquierda a derecha: inicia con
                         ingresos totales, luego se deducen los costos de
