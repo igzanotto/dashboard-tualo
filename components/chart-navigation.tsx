c26101ee55034935fdabdcb05e3fe00b41df8426
Manuel-latorre <manuel.latorre11@gmail.com>
2024-05-27 14:55:44 -0300

    active links

--- components/chart-navigation.tsx
+++ components/chart-navigation.tsx
@@ -1,17 +1,39 @@
-import { fetchReportById } from "@/lib/data";
+"use client"
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { fetchReportById } from "@/lib/data";
 
 interface ChartNavigationProps {
   reportId: string;
 }
 
-export default async function ChartNavigation({ reportId }: ChartNavigationProps) {
-  const report = await fetchReportById(reportId);
-  
+export default function ChartNavigation({ reportId }: ChartNavigationProps) {
+  const [report, setReport] = useState<any>(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const getReport = async () => {
+      const fetchedReport = await fetchReportById(reportId);
+      setReport(fetchedReport);
+    };
+
+    getReport();
+  }, [reportId]);
+
+  if (!report) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='flex items-center gap-4'>
       {report.charts.map((chart: any) => (
-        <Link key={chart.id} href={`/dashboard/reports/${reportId}/chart/${chart.id}`}>
+        <Link
+          key={chart.id}
+          href={`/dashboard/reports/${reportId}/chart/${chart.id}`}
+          className={pathname === `/dashboard/reports/${reportId}/chart/${chart.id}` ? 'bg-gradient-to-r from-[#4C30C5] to-[#39AEFF] text-white p-2 rounded-lg font-medium' : 'bg-gray-200 text-black p-2 rounded-lg font-medium'}
+        >
           {chart.type}
         </Link>
       ))}
