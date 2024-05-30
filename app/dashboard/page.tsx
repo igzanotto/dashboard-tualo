3cd4db3ea77cbd5fd9597c15e9f8bc45836b8ad9
Igzanotto <ignaciozanotto@gmail.com>
2024-05-30 17:42:48 -0300

    vercel bugfix leaving empty dashboard root

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,8 @@
-import ReportsPageDashboard from "./reports/page";
 
 export default function DashboardPage(){
     return(
         <div>
-            <ReportsPageDashboard/>
+
         </div>
     )
 }
