245b4a45f5602b0225d98cb89422060d278a8cbf
Manuel-latorre <manuel.latorre11@gmail.com>
2024-05-31 10:17:11 -0300

    add chart in report page if chart dosent exist

--- lib/actions.ts
+++ lib/actions.ts
@@ -281,6 +281,7 @@ export async function createChartEmbed(formData:FormData){
       }
 
       console.log('Inserted chart data:', newChart);
+      
       return newChart;
     }
   } catch (error) {
