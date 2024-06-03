9643eb9a090023f1cd8b3054c719c649e496a563
Manuel-latorre <manuel.latorre11@gmail.com>
2024-06-03 11:19:34 -0300

    show form when !chart in database

--- app/admin/businesses/[business_id]/reports/[report_id]/page.tsx
+++ app/admin/businesses/[business_id]/reports/[report_id]/page.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link';
 
 
 const chartOrder = [
-  'Cascada P&L',
+  'Ingresos y egresos',
   'Ventas',
   'Costos y gastos',
   'Utilidad neta',
@@ -129,93 +129,91 @@ export default async function ReportPage({
         </div>
       </form>
       <div className="mt-10 flex flex-col gap-36">
-          {orderedCharts.map((chart: any) => (
-            <div
-              className={`section-margin flex items-center justify-between gap-10 rounded-xl px-3 py-4 max-xl:flex-col 2xl:px-7 
-          ${
-            chart.type === 'Costos y gastos'
-              ? 'bg-[rgba(255,0,0,0.07046568627450978)]'
-              : chart.type === 'Ventas'
-              ? 'bg-[rgba(0,255,0,0.1)]'
-              : chart.type === 'Utilidad neta y margen neto'
-              ? 'bg-[rgba(163,98,238,0.1)]'
-              : chart.type === 'Márgenes'
-              ? 'bg-[rgba(219,103,255,0.1)]'
-              : chart.type === 'Gastos desglosados'
-              ? 'bg-[rgba(248,222,103,0.1)]'
-              : chart.type === 'Cascada P&L'
-              ? 'bg-[rgba(166,166,166,0.1)]'
-              : 'bg-gray-100'
-          }`}
-              id={chart.type}
-              key={chart.id}
-            >
-              <div className="max-xl:w-full xl:w-[50%] 2xl:w-[50%]">
-                <div className="flex items-center gap-2">
-                  <p className="my-4 text-xl font-semibold text-zinc-700 xl:text-2xl">
-                    {' '}
-                    Gráfica de <span>{chart.type}</span>
+  {chartOrder.map((type:any) => {
+    const chart = orderedCharts.find((chart: any) => chart.type === type);
+    return (
+      <div
+        className={`section-margin flex items-center justify-between gap-10 rounded-xl px-3 py-4 max-xl:flex-col 2xl:px-7 
+        ${
+          type === 'Costos y gastos'
+            ? 'bg-[rgba(255,0,0,0.07046568627450978)]'
+            : type === 'Ventas'
+            ? 'bg-[rgba(0,255,0,0.1)]'
+            : type === 'Utilidad neta y margen neto'
+            ? 'bg-[rgba(163,98,238,0.1)]'
+            : type === 'Márgenes'
+            ? 'bg-[rgba(219,103,255,0.1)]'
+            : type === 'Gastos desglosados'
+            ? 'bg-[rgba(248,222,103,0.1)]'
+            : type === 'Cascada P&L'
+            ? 'bg-[rgba(166,166,166,0.1)]'
+            : 'bg-gray-100'
+        }`}
+        id={type}
+        key={type}
+      >
+        <div className="max-xl:w-full xl:w-[50%] 2xl:w-[50%]">
+          <div className="flex items-center gap-2">
+            <p className="my-4 text-xl font-semibold text-zinc-700 xl:text-2xl">
+              {' '}
+              Gráfica de <span>{type}</span>
+            </p>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <InfoIcon width={20} height={20} />
+                </TooltipTrigger>
+                <TooltipContent className="w-[450px]">
+                  <p>
+                    Esta gráfica se lee de izquierda a derecha: inicia con
+                    ingresos totales, luego se deducen los costos de
+                    producción (los que están directamente relacionado con
+                    las ventas), revelando la utilidad bruta. A
+                    continuación, se restan los gastos operativos (los que
+                    son indirectos) para obtener la utilidad operativa.
+                    Por último se deducen los gastos financieros, para
+                    llegar a la utilidad neta. Las barras verdes suman y
+                    las rojas restan, dejando las grises como subtotales.
                   </p>
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>
-                        <InfoIcon width={20} height={20} />
-                      </TooltipTrigger>
-                      <TooltipContent className="w-[450px]">
-                        <p>
-                          Esta gráfica se lee de izquierda a derecha: inicia con
-                          ingresos totales, luego se deducen los costos de
-                          producción (los que están directamente relacionado con
-                          las ventas), revelando la utilidad bruta. A
-                          continuación, se restan los gastos operativos (los que
-                          son indirectos) para obtener la utilidad operativa.
-                          Por último se deducen los gastos financieros, para
-                          llegar a la utilidad neta. Las barras verdes suman y
-                          las rojas restan, dejando las grises como subtotales.
-                        </p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </div>
-                {chart.graphy_url ? (
-                  <ChartEmbed src={chart.graphy_url} />
-                ) : (
-                  <div>
-                    <h1 className='text-black'>Crear Gráfico de {chart.type}</h1>
-                    <form action={createChartEmbed} className='flex flex-col gap-4 xl:w-[60%] mt-10'>
-                      <input type="hidden" name="report_id" value={report.id} />
-                      <input type="hidden" name="type" value={chart.type} />
-                      <input
-                        type="text"
-                        name="graphy_url"
-                        placeholder="Url del gráfico"
-                        className="rounded-xl p-2"
-                      />
-                      <button
-                        type="submit"
-                        className="rounded-xl bg-blue-500 p-3 font-medium text-white"
-                      >
-                        Crear gráfico
-                      </button>
-                    </form>
-                  </div>
-                )}
-              </div>
-              <div className="rounded-lg bg-white px-3 py-5 xl:w-[50%] 2xl:w-[40%]">
-                {chart.insights && (
-                  <div>
-                    <h3 className="mb-5 text-center text-2xl font-medium">
-                      Resumen
-                    </h3>
-                    <p className="text-lg">
-                      {renderTextFromDatabase(chart.insights)}
-                    </p>
-                  </div>
-                )}
-              </div>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+          {chart ? (
+            <ChartEmbed src={chart.graphy_url} />
+          ) : (
+            <div>
+              <h1 className='text-black'>Crear Gráfico de {type}</h1>
+              <form action={createChartEmbed} className='flex flex-col gap-4 xl:w-[60%] mt-10'>
+                <input type="hidden" name="report_id" value={report.id} />
+                <input type="hidden" name="type" value={type} />
+                <input
+                  type="text"
+                  name="graphy_url"
+                  placeholder="Url del gráfico"
+                  className="rounded-xl p-2"
+                />
+                <button
+                  type="submit"
+                  className="rounded-xl bg-blue-500 p-3 font-medium text-white"
+                >
+                  Crear gráfico
+                </button>
+              </form>
             </div>
-          ))}
+          )}
         </div>
+        {chart && chart.insights && (
+          <div className="rounded-lg bg-white px-3 py-5 xl:w-[50%] 2xl:w-[40%]">
+            <h3 className="mb-5 text-center text-2xl font-medium">Resumen</h3>
+            <p className="text-lg">{renderTextFromDatabase(chart.insights)}</p>
+          </div>
+        )}
+      </div>
+    );
+  })}
+</div>
+
       </div>
     </div>
   );
