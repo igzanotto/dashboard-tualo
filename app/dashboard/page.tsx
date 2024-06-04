7eaa8b3886442ecaf30126cb27b1ebc6aae56e28
Igzanotto <ignaciozanotto@gmail.com>
2024-06-04 10:00:47 -0300

    redirect to last report

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,20 @@
-import ReportsPageDashboard from "./reports/page";
+import { getLastReport } from '@/lib/data';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function DashboardPage(){
-    return(
-        <div>
-            <ReportsPageDashboard/>
-        </div>
-    )
+export default async function Page() {
+
+  const report = await getLastReport();
+
+  if (report) {
+    const { id, month } = report;
+    return redirect(`/dashboard/reports/${id}/${month}`);
+  }
+
+  return (
+    <div>
+      <h1>Aun no tienes reportes</h1>
+    </div>
+  )
+
 }
