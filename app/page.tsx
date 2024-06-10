d1c5ef49fc4cf9f9ec5edbfac4b5203d06103245
Igzanotto <ignaciozanotto@gmail.com>
2024-06-10 12:44:08 -0300

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,66 @@
 'use client';
 
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation'; // Importación correcta para Next.js 13+ con App Router
+import { createClient } from '@/utils/supabase/client';
 
 
-export default function Page() {
+const SetPassword = () => {
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+  const [token, setToken] = useState('');
+  const supabase = createClient();
+
   useEffect(() => {
-    redirect('/admin')
-  }, [])
-  
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const token = params.get('access_token');
+    console.log('token', token);
+
+    if (token) {
+      setToken(token);
+    } else {
+      setError('Invalid or expired token.');
+    }
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!token) {
+      setError('Invalid or expired token.');
+      return;
+    }
+
+    const { data, error } = await supabase.auth.updateUser({
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      router.push('/login');
+    }
+  };
+
   return (
-   <>
-   </>
+    <div>
+      <h1>Set Your Password</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>New Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p>{error}</p>}
+        <button type="submit">Set Password</button>
+      </form>
+    </div>
   );
-}
+};
+
+export default SetPassword;
