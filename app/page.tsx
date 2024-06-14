61f750a146deb7ec90eccbef37b9e4ad067fd87f
Igzanotto <ignaciozanotto@gmail.com>
2024-06-14 17:45:04 -0400

    page

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
+
+
+export default function Page() {
+  useEffect(() => {
+    redirect('/admin')
+  }, [])
+
+  return (
+   <>
+   </>
+  );
+}
