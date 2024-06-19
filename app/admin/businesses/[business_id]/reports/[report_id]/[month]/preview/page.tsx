73e1134d1861cb3c122aced64891844a64a7b860
Igzanotto <ignaciozanotto@gmail.com>
2024-06-19 15:38:32 -0300

    markdown working

--- app/admin/businesses/[business_id]/reports/[report_id]/[month]/preview/page.tsx
+++ app/admin/businesses/[business_id]/reports/[report_id]/[month]/preview/page.tsx
@@ -26,6 +26,7 @@ import { uploadImage, uploadImageChart } from '@/lib/actions';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import CompareResultsTooltip from '@/components/tooltips/compare-results';
 import CompareMarginsTooltip from '@/components/tooltips/compare-margins';
+import MarkdownRenderer from '@/components/markdownRendered';
 
 const libreBaskerville = Libre_Baskerville({
   subsets: ['latin'],
@@ -60,67 +61,6 @@ export default async function PreviewPage({
   const { business_id } = params;
 
   const orderedCharts = reorderCharts(report.charts);
-
-  const renderTextFromDatabase = (text: string | undefined) => {
-    if (!text) {
-      return <p>Vacío</p>;
-    }
-
-    const applyStyles = (text: string) => {
-      return <span className="font-bold text-[#003E52]">{text}</span>;
-    };
-
-    const paragraphs = text.split('\n');
-    const formattedParagraphs = paragraphs.map((paragraph, index) => {
-      const lines = paragraph.split('\n');
-      const formattedLines = lines.map((line, lineIndex) => {
-        const [firstPart, ...rest] = line.split(':');
-        const secondPart = rest.join(':').trim();
-
-        return (
-          <span key={lineIndex}>
-            {applyStyles(firstPart)}
-            {secondPart && `: ${secondPart}`} <br />
-          </span>
-        );
-      });
-
-      return <span key={index}>{formattedLines}</span>;
-    });
-
-    return <>{formattedParagraphs}</>;
-  };
-
-  const processText = (text: string) => {
-    const regex = /(\#\#\#\# .+? (.*))|(\*\*[^*]+\*\*)|([^\*#]+)/g;
-    const matches = text.match(regex);
-  
-    return matches?.map((match, index) => {
-      if (match.startsWith('####')) {
-        return (
-          <p key={index} className="font-medium text-2xl mb-4">
-            {match.replace('#### ', '')}
-          </p>
-        );
-      } else if (match.startsWith('**')) {
-        return (
-          <span key={index} className="font-semibold">
-            {match.replace(/\*\*/g, '')}
-          </span>
-        );
-      } else if (match.includes(':')) {
-        return (
-          <span key={index}>
-            {match}
-          </span>
-        );
-      } else {
-        return <span key={index}>{match}</span>;
-      }
-    });
-  };
-
-  
 
   return (
     <div className="flex flex-col gap-3 xl:px-2">
@@ -177,12 +117,12 @@ export default async function PreviewPage({
             </div>
             <div className="rounded-xl bg-[#003E52]/10 p-3 text-[#003E52]">
               {!report.business_resume ? (
-                <div className='whitespace-pre-wrap'>
-                  {processText(report.operations_resume)}
+                <div className="whitespace-pre-wrap">
+                  <MarkdownRenderer markdown={report.operations_resume} />
                 </div>
               ) : (
-                <div className='whitespace-pre-wrap'>
-                  {processText(report.business_resume)}
+                <div className="whitespace-pre-wrap">
+                  <MarkdownRenderer markdown={report.business_resume} />
                 </div>
               )}
             </div>
@@ -192,9 +132,10 @@ export default async function PreviewPage({
               Metas financieras
             </p>
             <div className="rounded-xl bg-[#003E52]/10 p-3 text-[#003E52]">
-            <div className='whitespace-pre-wrap'>
-                  {processText(report.goals)}
-                </div>
+              <div className="whitespace-pre-wrap">
+                {' '}
+                <MarkdownRenderer markdown={report.goals} />
+              </div>
             </div>
           </div>
         </div>
@@ -204,73 +145,73 @@ export default async function PreviewPage({
       <div className="mt-10 flex flex-col gap-36">
         {orderedCharts.map((chart: any) => (
           <div
-          className={`section-margin flex items-center justify-between rounded-xl bg-[#003E52]/10 px-3 py-4 max-xl:flex-col 2xl:px-7`}
-          id={chart.type}
-          key={chart.id}
-        >
-          <div className="max-xl:w-full">
-            <div className="flex items-center gap-2">
-              <p className="text-xl font-semibold text-[#003E52] xl:text-2xl">
-                {' '}
-                Gráfica de <span>{translateChartType(chart.type)}</span>
-              </p>
-              {chart.type === 'waterfall' ? (
-                    <WaterfallTooltip />
-                  ) : chart.type === 'actual_vs_average' ? (
-                    <CompareResultsTooltip />
-                  ) : chart.type === 'actual_vs_average_2' ? (
-                    <CompareMarginsTooltip />
-                  ) : chart.type === 'sales' ? (
-                    <SalesTooltip />
-                  ) : chart.type === 'costs_and_expenses' ? (
-                    <CostsExpensesTooltip />
-                  ) : chart.type === 'net_profit_and_margins' ? (
-                    <ProfitMarginsTooltip />
-                  ) : chart.type === 'margins' ? (
-                    <MarginsTooltip />
-                  ) : chart.type === 'detailed_expenses' ? (
-                    <ExpensesTooltip />
-                  ) : (
-                    <p>Este grafico no tiene tooltip</p>
-                  )}
-            </div>
-            <div className="flex items-center gap-10 max-xl:flex-col">
-              <Dialog>
-                <DialogTrigger>
-                  <img
-                    src={chart.graphy_url}
-                    alt={chart.type}
-                    width={1000}
-                    height={1000}
-                    className="mx-auto my-5 h-[100%] rounded-xl xl:w-[1000px] max-xl:w-full"
-                  />
-                </DialogTrigger>
-                <DialogContent>
-                  <img
-                    src={chart.graphy_url}
-                    alt={chart.type}
-                    width={2000}
-                    height={1000}
-                    className="mx-auto h-full w-full rounded-xl"
-                  />
-                </DialogContent>
-              </Dialog>
-              <div className="w-full rounded-lg bg-white px-3 py-5 xl:h-[450px] 2xl:h-full xl:w-[50%] xl:overflow-y-auto 2xl:w-[40%] max-md:h-[400px] max-md:overflow-y-auto">
-                {chart.insights && (
-                  <div className="flex flex-col justify-between">
-                    <h3 className="mb-5 text-center text-2xl font-medium">
-                      Análisis
-                    </h3>
-                    
-                    <div className='whitespace-pre-wrap text-[#003E52]'>
-                    {processText(chart.insights)}
-                    </div>
-                  </div>
+            className={`section-margin flex items-center justify-between rounded-xl bg-[#003E52]/10 px-3 py-4 max-xl:flex-col 2xl:px-7`}
+            id={chart.type}
+            key={chart.id}
+          >
+            <div className="max-xl:w-full">
+              <div className="flex items-center gap-2">
+                <p className="text-xl font-semibold text-[#003E52] xl:text-2xl">
+                  {' '}
+                  Gráfica de <span>{translateChartType(chart.type)}</span>
+                </p>
+                {chart.type === 'waterfall' ? (
+                  <WaterfallTooltip />
+                ) : chart.type === 'actual_vs_average' ? (
+                  <CompareResultsTooltip />
+                ) : chart.type === 'actual_vs_average_2' ? (
+                  <CompareMarginsTooltip />
+                ) : chart.type === 'sales' ? (
+                  <SalesTooltip />
+                ) : chart.type === 'costs_and_expenses' ? (
+                  <CostsExpensesTooltip />
+                ) : chart.type === 'net_profit_and_margins' ? (
+                  <ProfitMarginsTooltip />
+                ) : chart.type === 'margins' ? (
+                  <MarginsTooltip />
+                ) : chart.type === 'detailed_expenses' ? (
+                  <ExpensesTooltip />
+                ) : (
+                  <p>Este grafico no tiene tooltip</p>
                 )}
+              </div>
+              <div className="flex items-center gap-10 max-xl:flex-col">
+                <Dialog>
+                  <DialogTrigger>
+                    <img
+                      src={chart.graphy_url}
+                      alt={chart.type}
+                      width={1000}
+                      height={1000}
+                      className="mx-auto my-5 h-[100%] rounded-xl max-xl:w-full xl:w-[1000px]"
+                    />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <img
+                      src={chart.graphy_url}
+                      alt={chart.type}
+                      width={2000}
+                      height={1000}
+                      className="mx-auto h-full w-full rounded-xl"
+                    />
+                  </DialogContent>
+                </Dialog>
+                <div className="w-full rounded-lg bg-white px-3 py-5 max-md:h-[400px] max-md:overflow-y-auto xl:h-[450px] xl:w-[50%] xl:overflow-y-auto 2xl:h-full 2xl:w-[40%]">
+                  {chart.insights && (
+                    <div className="flex flex-col justify-between">
+                      <h3 className="mb-5 text-center text-2xl font-medium">
+                        Análisis
+                      </h3>
+
+                      <div className="whitespace-pre-wrap text-[#003E52]">
+                        <MarkdownRenderer markdown={chart.insights} />
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
         ))}
       </div>
 
@@ -283,8 +224,8 @@ export default async function PreviewPage({
           />
           {/* <p className="mb-4 text-2xl font-semibold text-[#003E52]">Conclusiones</p> */}
           <div className="mt-16 rounded-xl bg-[#003E52]/10 p-3 text-[#003E52] max-md:mx-auto max-md:w-[96%]">
-            <div className='whitespace-pre-wrap'>
-              {processText(report.analysis)}
+            <div className="whitespace-pre-wrap">
+              <MarkdownRenderer markdown={report.analysis} />
             </div>
           </div>
         </div>
@@ -302,10 +243,7 @@ export default async function PreviewPage({
                 key={index}
                 className="flex flex-col rounded-xl bg-[#003E52]/10 p-4 text-[#003E52]"
               >
-                <div className='whitespace-pre-wrap'>
-                  {processText(`${data.content}`)}
-                </div>
-                
+                <div className="whitespace-pre-wrap"><MarkdownRenderer markdown={data.content} /></div>
               </div>
             ))}
           </div>
---
+++ components/markdownRendered.tsx
@@ -0,0 +1,14 @@
+import { marked } from 'marked';
+
+interface MarkdownRendererProps {
+  markdown: string;
+}
+
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ markdown }) => {
+  // Use the parse method to convert markdown to HTML
+  const htmlContent = marked(markdown);
+
+  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+};
+
+export default MarkdownRenderer;
