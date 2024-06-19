063db281a622ca8f0a01c17383ed23e51056251d
Igzanotto <ignaciozanotto@gmail.com>
2024-06-19 10:44:37 -0300

    cta removed effect

--- components/landing/components/Buttons/CallToAction.tsx
+++ components/landing/components/Buttons/CallToAction.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CallToAction = () => {
   return (
-        <Link data-aos="zoom-in-up" href={"https://calendly.com/jorge-tualo/business-initial-interview"} target="_blank" className="bg-[#ff6c0e] text-white font-semibold mt-8 w-[230px] max-lg:self-center p-3 rounded-xl text-center hover:bg-[#ff9655] transition-all">
+        <Link href={"https://calendly.com/jorge-tualo/business-initial-interview"} target="_blank" className="bg-[#ff6c0e] text-white font-semibold mt-8 w-[230px] max-lg:self-center p-3 rounded-xl text-center hover:bg-[#ff9655] transition-all">
           ¡Quiero probarlo!   
         </Link>
   )
