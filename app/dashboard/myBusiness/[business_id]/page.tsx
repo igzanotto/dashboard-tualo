23fb1256edb734d50cf44592f903de5df7204e3b
Igzanotto <ignaciozanotto@gmail.com>
2024-06-27 17:02:20 -0300

    stripe working

--- app/dashboard/myBusiness/[business_id]/page.tsx
+++ app/dashboard/myBusiness/[business_id]/page.tsx
@@ -1,17 +1,31 @@
-import Movements from '@/components/movements/movements';
+import ButtonCheckout from "@/components/dashboard/ButtonCheckout";
+import { Stripe } from "stripe";
 
-interface MyBusinessPageProps {
-  params: {
-    business_id: string;
-  };
-};
+async function loadPrices() {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const prices = await stripe.prices.list();
+  const sortedPrices = prices.data.sort(
+    (a, b) => (a.unit_amount ?? 0) - (b.unit_amount ?? 0)
+  );
+  return sortedPrices;
+}
 
-
-export default function MyBusinessPage({ params }: MyBusinessPageProps) {
-  const { business_id } = params;
-
+async function PricingPage() {
+  const prices = await loadPrices();
+  console.log(prices);
+  const price = prices[0];
 
   return (
-    <h1>business</h1>
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        
+              <ButtonCheckout priceId={price.id} />
+           
+
+    
+      </div>
+    </div>
   );
 }
+
+export default PricingPage;
