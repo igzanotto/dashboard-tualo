db5cbf332ceb3e9bb71841f93a1ab39616819bca
Igzanotto <ignaciozanotto@gmail.com>
2024-07-01 09:19:18 -0300

    mp falta el callback y update en db

--- app/dashboard/(callbacks)/success/page.tsx
+++ app/dashboard/(callbacks)/success/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+"use client"
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react';
 
 const SuccessPage: React.FC = () => {
+    const searchParams = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+    const status = params.get('status')
+
+    // it updates the user status in the db to active using data actions
+    
+    
+    
+
     return (
         <div>
             <h1>Payment Successful!</h1>
             <p>Thank you for your subscription payment.</p>
-            {/* Add any additional content or components here */}
         </div>
     );
 };
--- app/dashboard/myBusiness/[business_id]/page.tsx
+++ app/dashboard/myBusiness/[business_id]/page.tsx
@@ -1,11 +1,13 @@
-import ButtonCheckout from "@/components/dashboard/ButtonCheckout";
-import { Stripe } from "stripe";
+import ButtonCheckout from '@/components/dashboard/ButtonCheckout';
+import { Stripe } from 'stripe';
 
 async function loadPrices() {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2024-06-20',
+  });
   const prices = await stripe.prices.list();
   const sortedPrices = prices.data.sort(
-    (a, b) => (a.unit_amount ?? 0) - (b.unit_amount ?? 0)
+    (a, b) => (a.unit_amount ?? 0) - (b.unit_amount ?? 0),
   );
   return sortedPrices;
 }
@@ -16,13 +18,9 @@ async function PricingPage() {
   const price = prices[0];
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <div>
-        
-              <ButtonCheckout priceId={price.id} />
-           
-
-    
+        <ButtonCheckout priceId={price.id} />
       </div>
     </div>
   );
